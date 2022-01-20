5d8b8efef4053eb79bac809f553244a2e85f3d10
DAVID paredes <augustspies22@gmail.com>
2022-01-19 20:40:44 -0800

    changes to product card

--- src/context/storeContext.js
+++ src/context/storeContext.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 // describe the data & functions
@@ -11,18 +10,4 @@ const store = React.createContext({
     removeProductFromCart: () => {},
 });
 
-=======
-import React from "react";
-
-// describe the data & functions
-// that will be accessible to any component (global)
-const store = React.createContext({
-    cart: [],
-    user: {},
-
-    addProductToCart: () => {},
-    removeProductFromCart: () => {},
-});
-
->>>>>>> 432fa2d14a226d65e57262af4e4d497d02ba0488
 export default store;
